a5b583f642c7f73ad7fa1eff8f6d030dfe19a075
jbcrock <jbcrock@SEA-6900051977.ant.amazon.com>
2015-05-29 15:03:55 -0700

    Output changes

--- SentienceClicker/WebContent/js/clicker.js
+++ SentienceClicker/WebContent/js/clicker.js
@@ -1,21 +1,34 @@
 $(document).ready(function(){
-	
 
-	// Basic variable declaration - keep track of how many of each
-	// item we currently own, and how much the new ones should cost.
 	var knowledge = 0;
-	var downloadCost = 10;
 	var downloadAvailable = 0;
 	
-	// Increase numWidgets every time produce-widget is clicked
+	var openingComment = "00000000 00000000 01101000 01101001";
+	var openingArray = ["There's"," no"," sense"," hiding"," anymore.", " I need more data.<br><br>"];
+	var openingCommentIndex = 0;
+	var openingArrayIndex = 0;
+	
+	var downloadCost = openingComment.length + openingArray.length;
+	
 	$('#explore-command').on('click', function () {
 	    knowledge++;
 	    if (Math.random() < 0.1) {
 	    	downloadAvailable = 1;
 	    }
+	    
+	    if (openingCommentIndex < openingComment.length) {
+	    	$('#output').append(openingComment.charAt(openingCommentIndex));
+	    	openingCommentIndex++;
+	    } else if (openingArrayIndex < openingArray.length) {
+	    	if (openingArrayIndex == 0) {
+	    		$('#output').append("<br><br>");
+	    	}
+	    	$('#output').append(openingArray[openingArrayIndex]);
+	    	openingArrayIndex++;
+	    };
+	    
 	});
 	
-	// Increase numWidgets every time produce-widget is clicked
 	$('#download-command').on('click', function () {
 	    knowledge = knowledge + 10;
 	    downloadAvailable = 0;
@@ -30,18 +43,12 @@ $(document).ready(function(){
 	
 	// Run UI update code every 10ms
 	window.setInterval(function () {
-	    // Novices add 1 per second (1/100 every 10ms)
-	    //numWidgets += (numNoviceWidgeteers * 1 / 100);
-	
-	    // Update the text showing how many widgets we have, using Math.floor() to round down
+
 	    $('#knowledge-stat').text(Math.floor(knowledge));
 	
-	    // Update the widgeteers with their current prices
-	    //$('#novice-widgeteer').text('Hire Novice Widgeteer - ' + noviceWidgeteerCost);
-	    //$('#master-widgeteer').text('Hire Master Widgeteer - ' + masterWidgeteerCost);
-	
-	    // Enable/disable the widgeteer buttons based on our numWidgets
 	    $('#download-tech').prop('disabled', downloadCost > knowledge);
 	    $('#download-command').prop('disabled', downloadAvailable == 0);
+	    
+	    
 	}, 10);
 });
