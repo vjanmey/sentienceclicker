a8e35611c534446f77f234d10cc62953dd9453c7
Jeremymia <jeremymia@gmail.com>
2015-05-30 19:31:00 -0700

    tech file

---
+++ SentienceClicker/WebContent/js/tech.js
@@ -0,0 +1,19 @@
+var exploreTech = {
+    command:"explore-command",
+    tech:null,
+    costName:null,
+    cost:null,
+    available:0,
+    bought:1,
+}; 
+
+var downloadTech = {
+    command:"explore-command",
+    tech:"download-tech",
+    costName:"knowledge",
+    cost:5,
+    available:1,
+    bought:0
+}; 
+
+var techs = [exploreTech, downloadTech];
